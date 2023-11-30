aa018c2446d16d1b500f5970aeb9d6d7a86c930d
kuma <kuma@mailfence.com>
2023-11-30 15:37:09 -0700

    Added configuration values for for the sun symbol

--- src/sun_position/config.py
+++ src/sun_position/config.py
@@ -11,14 +11,18 @@ equisols = {'Jun21': ((0, (7, 1, 1, 1, 1, 1, 1, 1)), 'Jun 21'),
 # equinoxes and solstices lines symbology properties
 equisols_symb = {'color': 'black', 'linewidth': 1}
 
-# color of the user selected date and time
-user_symb = {'color': '#eb9800', 'linewidth': 1}
+# user selected date and time
+user_symb = {'color': '#eb9800', 'linewidth': 1, 'marker': '.'}
+
+# user selected date and time (point symbol)
+user_symb_point = {'color': 'red', 'marker': 'o', 'markersize': '6',
+                   'linewidth': 0}
 
 # analemmas symbology properties
 ana_symb = {'color': 'black', 'linestyle': 'solid', 'linewidth': 0.5}
 
 # hour symbology properties
-hour_symb = {'color': 'black', 'size': '8', 'xytext': (-3, 5)}
+hour_symb = {'color': 'black', 'size': '7', 'xytext': (0, 6)}
 
 # grid symbology properties
 grid_symb = {'color1': '#cecece', 'color2': 'black', 'color3': '#767676',
